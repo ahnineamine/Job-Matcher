a02b116fc5c6334e1948e85e4745b4b486a9fffe
Amine Ahnine <ahnineamine6@gmail.com>
2019-03-27 10:40:42 +0000

    committed

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from DocSim import DocSim
 
 #Initiate Flask application
 app = flask.Flask(__name__)
-#app.config["DEBUG"] = True
 
 #load word embeddings model
 model_path = "./models/word2vec_model_optim"
@@ -17,36 +16,7 @@ model = KeyedVectors.load(model_path)
 
 #initiate DocSim object
 ds = DocSim(model)
-"""
----this part is for Json file input---
 
-L_input=[]
-L_index=[]
-data = json.loads(open('test-data.json').read()) #name of json file in general
-for cv in data['cv']:
-    index = cv['id']
-    content = cv['exp']+','+ cv['skill']
-    L_input.append(content)   
-    L_index.append(index)
-
-content_offer = data['job']['title']+','+data['job']['desc']+','+data['job']['req']
-L_input.append(content_offer)
-
-"""
-def get_data(file):
-    with open(file) as f:
-        line_list=[]
-        index_list = []
-        for index,l in enumerate(f):
-            line = l.rstrip('\n')
-            line_uni = unicodedata.normalize("NFKD",line)
-            line_list.append(line_uni)
-            index_list.append(index)
-    return line_list,index_list
-
-
-#L_input,L_index= get_data('data/validation_data/validation_set_ITT.csv')
-@app.route('/')
 def index():
     return 'Hello World!'
 
@@ -57,12 +27,10 @@ def process_files():
     L_index=[]
     for cv in data['cv']:
         index = cv['id']
-        #content = cv['exp']+' , '+ cv['skill']
         content = cv['info']
         L_input.append(content)   
         L_index.append(index)
 
-    #content_offer = data['job']['title']+' , '+data['job']['desc']+' , '+data['job']['req']
     for job in data['job']:
         job_content = job['jobinfo']
         L_input.append(job_content)
@@ -73,8 +41,4 @@ def process_files():
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5000))
    app.run(host='0.0.0.0', port=port, debug=True)
-
-#sim_scores = ds.calculate_similarity(L_input,L_index)
-
-#print(sim_scores)
 
